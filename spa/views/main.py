eeee0f5bb002fc7a03fd47322765919046191b17
William Di Cicco <nearata@protonmail.com>
2020-07-25 16:14:44 +0200

    Slash is not needed

--- spa/views/main.py
+++ spa/views/main.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 bp = Blueprint("main", __name__, static_folder="../static", template_folder="../templates")
 
 
-@bp.route("/", defaults={"path": "/"})
+@bp.route("/", defaults={"path": ""})
 @bp.route("/<path:path>")
 def catch_all(path):
     return render_template("index.html")
